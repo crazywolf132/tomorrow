ebfe807a70482beea8d31a1839e7685788bd226f
Brayden Moon <crazywolf132@gmail.com>
2022-12-08 15:59:06 +1100

    fix (metro): now exports metro-babel-transformer

--- packages/@tomorrow/metro/tsup.config.ts
+++ packages/@tomorrow/metro/tsup.config.ts
@@ -2,9 +2,22 @@
 import { defineConfig } from 'tsup';
 import { tsupBuilder } from 'build-env';
 
-export default defineConfig([tsupBuilder({
-    entry: ['src/index.ts'],
-    reactNative: false,
-    cloneFile: false,
-    external: ['@tomorrow/metro'] // this is thanks to the import
-})])
+export default defineConfig(
+    [
+        tsupBuilder({
+            entry: ['src/index.ts'],
+            reactNative: false,
+            cloneFile: true,
+            external: ['@tomorrow/metro', 'babel-preset-tomorrow'] // this is thanks to the import
+        }),
+        tsupBuilder({
+            name: 'metro-babel-transformer',
+            entry: ['src/metro-babel-transformer.ts'],
+            external: ['@tomorrow/metro', 'babel-preset-tomorrow'],
+            format: 'cjs',
+            dts: false,
+            reactNative: false,
+            cloneFile: true
+        })
+    ]
+)
