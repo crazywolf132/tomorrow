f9868da233556f7702b1cce04c2090db01c8ac2d
Brayden Moon <crazywolf132@gmail.com>
2022-12-08 15:56:03 +1100

    fix: updated server to actually run

--- packages/@tomorrow/server/src/index.ts
+++ packages/@tomorrow/server/src/index.ts
@@ -2,20 +2,23 @@
 import express from 'express';
 import morgan from 'morgan';
 import { Server } from 'http';
-import { importFromProject } from "toolbelt";
+import { importFromProject, importMetroFromProject } from "toolbelt";
 import { createDevServerMiddleware } from "./createDevServerMiddleware";
 import Log from 'debug';
 const debug = Log.get('server');
 
 export const runMetroServerAsync = async (projectRoot: string = process.cwd(), options: any) => {
-    // const Metro = importMetroFromProject(projectRoot);
-    debug('getting tomorrow metro config')
+    const Metro = importMetroFromProject(projectRoot);
+    debug('metro:', 'getting tomorrow metro config')
     const tomorrowMetroConfig = importFromProject(projectRoot, '@tomorrow/metro');
-    debug('getting metro config')
+    debug('metro:', 'getting metro config')
     const metroConfig = await tomorrowMetroConfig.loadAsync(projectRoot, { ...options });
 
+    debug('metro:', 'creating middleware')
+    const connectMiddleware = await Metro.createConnectMiddleware(metroConfig);
+
     debug('getting devServer middleware')
-    const { eventsSocketEndpoint, websocketEndpoints, messageSocketEndpoint, middleware } = createDevServerMiddleware(projectRoot, {
+    const { eventsSocketEndpoint, websocketEndpoints, messageSocketEndpoint } = createDevServerMiddleware(projectRoot, {
         port: metroConfig.server.port,
         watchFolders: metroConfig.watchFolders
     });
@@ -27,14 +30,15 @@ export const runMetroServerAsync = async (projectRoot: string = process.cwd(), o
     debug('express:', 'adding morgan')
     app.use(morgan('dev'));
     debug('express:', 'adding middleware');
-    app.use(middleware);
+    app.use(connectMiddleware.middleware);
 
     debug('server:', 'starting');
     const server = new Server(app);
     debug('server:', 'listening')
     server.listen(metroConfig.server.port);
 
-    // debug('attaching server tyo HMR')
+    debug('attaching server tyo HMR');
+    connectMiddleware.attachHmrServer(server);
     // middleware.attachHmrServer(server);
 
     return {
